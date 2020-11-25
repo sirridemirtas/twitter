2b04f65775cec598ca421a90beda0cf6b99b2ac4
Sirri Demirtas <sirridemirtas@gmail.com>
2020-11-25 17:44:38 +0300

    add meta tags

--- components/layout/index.js
+++ components/layout/index.js
@@ -8,9 +8,10 @@ import Main from './cols/main'
 import Extra from './cols/extra'
 import useWindowSize from "../../hooks/useWindowSize"
 import CONSTS from '../../constants/index'
-import PageTitle from "../common/PageTitle/page-title"
+import PageTitle from "../common/PageTitle"
 import TrendTopics from "../common/TrendTopics"
-import SearchBox from "../common/SearchBox/search-box"
+import SearchBox from "../common/SearchBox"
+import FooterLinks from "../common/FooterLinks"
 
 function Layout({ children, ...props }) {
 	const size = useWindowSize();
@@ -19,6 +20,13 @@ function Layout({ children, ...props }) {
 			<title>{props.pageTitle} / Twitter</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<meta name="google" value="notranslate" />
+			<meta name="description" content="Twitter Web App Clone With ReactJS & NextJS" />
+			<meta name="robots" content="index, follow" />
+
+			<meta property="og:image" content="/preview.png" />
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:creator" content="@sirridemirtas" />
+			<meta property="twitter:image" content="/preview.png" />
 		</Head>
 
 		<Sidebar flat={size.width < CONSTS.DESKTOP_SIZE}></Sidebar>
@@ -29,6 +37,7 @@ function Layout({ children, ...props }) {
 		{size.width > CONSTS.TABLET_SIZE && <Extra>
 			<SearchBox />
 			<TrendTopics />
+			<FooterLinks />
 		</Extra>}
 	</div>
 }
